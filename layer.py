a7befaed23d539f6495ae3553128e013d1d66e1e
David England <david.england.de@googlemail.com>
2024-01-08 11:09:39 +0000

    Single layer: initialise and implement feed-forward

--- layer.py
+++ layer.py
@@ -2,5 +2,19 @@ import numpy as np
 
 
 class Layer:
-    def __init__(self):
-        pass
+    def __init__(self, n: int, n_prev: int, sigma, d_sigma):
+        self.n_in = n_prev
+        self.n_out = n
+
+        self.sigma = sigma
+        self.d_sigma = d_sigma
+
+        np.random.seed(4)
+        self.w = np.random.randn(n, n_prev)
+    
+    def run(self, input: np.ndarray):
+        if input.shape != (self.n_in, 1):
+            raise ValueError("Layer received array of incorrect size.")
+        
+        self.z = np.dot(self.w, input)
+        self.a = np.apply_along_axis(self.sigma, 0, self.z)
