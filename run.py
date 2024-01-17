8e958343d161b8e70bd64cdb4d0752588223ae11
David England <david.england.de@googlemail.com>
2024-01-17 17:26:24 +0000

    Include batching in sample run code

--- run.py
+++ run.py
@@ -6,16 +6,16 @@ x, y = make_classification(n_samples=100, n_features=4, n_informative=4, n_redun
 
 sigma = lambda x: x if x > 0 else 0
 d_sigma = lambda y: 1 if y > 0 else 0
-d_cost = lambda y_true, y_pred: np.array([-2. * (y_true[0, 0] - y_pred[0, 0])]).reshape(1, 1)
+d_cost = lambda y_true, y_pred: -2. * (y_true - y_pred).T
 
-n = Net(4, [4, 1], [sigma, sigma], [d_sigma, d_sigma], d_cost)
+n = Net(4, [4, 1], [sigma, sigma], [d_sigma, d_sigma], d_cost, batch_size=2)
 
 print("\nWeights BEFORE")
 print(n.layers[0].w)
 print(n.layers[1].w)
 print(f"Max weights: {np.abs(n.layers[0].w).max()}, {np.abs(n.layers[1].w).max()}")
 
-n.train(x[0:1, :].T, y.reshape(100, 1)[0:1, :])
+n.train(x[:2, :].T, y.reshape(1, 100)[:, :2])
 
 print("\nInput/output values")
 print(n.layers[0].a_in)
