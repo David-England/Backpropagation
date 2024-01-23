ce762a8476407036690c895b09556151b4215802
David England <david.england.de@googlemail.com>
2024-01-23 09:53:59 +0000

    Update sample problem

--- run.py
+++ run.py
@@ -1,33 +1,23 @@
 import numpy as np
 from sklearn.datasets import make_classification
-from backpropagation import Net, ReLU
+from sklearn.model_selection import train_test_split
+from backpropagation import Net, Sigmoid
 
-x, y = make_classification(n_samples=100, n_features=4, n_informative=4, n_redundant=0, random_state=4)
+# ---- 1. LOADING
+x, y = make_classification(n_samples=8192, n_features=4, n_informative=4, n_redundant=0, random_state=4)
 
-n = Net(4, [4, 1], [ReLU, ReLU], lambda y_true, y_pred: -2. * (y_true - y_pred).T, batch_size=2)
+# ---- 2. CLEANING
+# none required
 
-print("\nWeights BEFORE")
-print(n.layers[0].w)
-print(n.layers[1].w)
-print(f"Max weights: {np.abs(n.layers[0].w).max()}, {np.abs(n.layers[1].w).max()}")
+# ---- 3. TRAIN-TEST SPLIT
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=4)
 
-n.train(x[:2, :].T, y.reshape(1, 100)[:, :2])
+# ---- 4. FITTING
+n = Net(4, [4, 1], [Sigmoid, Sigmoid], lambda y_true, y_pred: -2. * (y_true - y_pred).T, batch_size=32)
+n.train(x_train.T, y_train.reshape(1, -1))
 
-print("\nInput/output values")
-print(n.layers[0].a_in)
-print(n.layers[1].a_in)
+# ---- 5. PREDICTING
+filter_point5 = np.vectorize(lambda x: 1 if x > .5 else 0)
+predicted = filter_point5(n.run(x_test.T))
 
-print("\nLinear results (z)")
-print(n.layers[0].z)
-print(n.layers[1].z)
-
-print("\nWeights")
-print(n.layers[0].w)
-print(n.layers[1].w)
-print(f"Max weights: {np.abs(n.layers[0].w).max()}, {np.abs(n.layers[1].w).max()}")
-
-print("\nWeight gradients")
-print(n.layers[0].dc_dw)
-print(n.layers[1].dc_dw)
-
-print(f"\ny (actual): {y[0]}")
+print(predicted.reshape(-1)[:25], y_test[:25])
