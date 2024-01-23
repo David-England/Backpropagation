6acef7c334c9e8aebf46f2925d4d663e311bf037
David England <david.england.de@googlemail.com>
2024-01-23 17:22:35 +0000

    Refactor: redundant import statement

--- run_breast_cancer.py
+++ run_breast_cancer.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
